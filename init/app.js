31b3451703dc66e4e82aff9dd5efc47ef90e1e35
mujupatel41 <mujupatel41@gmail.com>
2024-07-12 11:42:39 +0530

    Data was Initialized

---
+++ init/app.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Listing = require("../models/listingModel.js");
+const initData = require("./data1.js");
+const MONGO_URL = "mongodb://127.0.0.1:27017/Wanderlust";
+
+main().then((res)=>{
+    console.log("Data Initialize Successfully");
+}).catch(err => console.log(err));
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+};
+
+const initDB = async () =>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+};
+
+// initDB();
