da09ecd871e9bd0317e6826355c99037360b487f
mujupatel41 <mujupatel41@gmail.com>
2024-07-15 11:16:05 +0530

    Index Route

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const mongoose = require("mongoose");
+const path = require("path");
 const Listing = require("./models/listingModel.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Wanderlust";
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 main().then((res)=>{
     console.log("Connected to DB");
@@ -33,6 +37,14 @@ app.get("/", (req, res)=>{
 //     data.save();
 //     res.send("Data Collected");
 // });
+
+// Index Route
+
+app.get("/listings", async (req, res)=>{
+    let allListings = await Listing.find();
+
+    res.render("listings/index.ejs", {listings: allListings});
+});
 
 app.listen(port, ()=>{
     console.log(`Server is listening to port ${port}`);
