b9a613ce5b6d12b9c021565523a66ac2dfa60a87
mujupatel41 <mujupatel41@gmail.com>
2024-09-04 11:41:34 +0530

    Configuring Strategy

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const ejsMate = require("ejs-mate");
 const path = require("path");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const ExpressError = require("./utils/ExpressError.js");
 
@@ -35,6 +38,12 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.engine("ejs", ejsMate);
 
