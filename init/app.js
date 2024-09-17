106bc82362add2dc5849fc3b158f24baba59b6df
mujupatel41 <mujupatel41@gmail.com>
2024-09-17 16:14:13 +0530

    Displaying images in landing page and show page

--- init/app.js
+++ init/app.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Listing = require("../models/listingModel.js");
-const initData = require("./data1.js");
-const MONGO_URL = "mongodb://127.0.0.1:27017/Wanderlust";
+const initData = require("./data2.js");
+const MONGO_URL = "mongodb://127.0.0.1:27017/Airbnb";
 
 main().then((res)=>{
     console.log("Data Initialize Successfully");
@@ -13,7 +13,7 @@ async function main(){
 
 const initDB = async () =>{
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "66daf1a420fd65684df71203"}));
+    initData.data = initData.data.map((obj) => ({...obj, owner: "66e95a3a0939eb9406a7ca69"}));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized");
 };
